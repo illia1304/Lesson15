51a041ee53f978ad3ba06dfb7ba822d73825a330
Illia <1304illia@gmail.com>
2023-10-09 16:37:44 +0300

    Add project files.

---
+++ Lesson15/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lesson14
+{
+    public class Stack<T>
+    {
+        public Stack() { }
+
+        public List<T> stack = new List<T>();
+        public void Push()
+        {
+
+        }
+    }
+    class Programme
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
